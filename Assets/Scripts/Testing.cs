dfc9fa0049a348f32ed617322944e0a8bfdb54a9
Ryan Churchill <ryan.churchill@gmail.com>
2022-07-18 17:37:09 -0700

    28. grid system

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private GridSystem gridSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gridSystem = new GridSystem(10, 10, 2f);
+
+        Debug.Log(new GridPosition(5, 7));
+    }
+
+    private void Update()
+    {
+        MouseWorld mouseWorld = MouseWorld.GetInstance();
+
+        Debug.Log(
+            gridSystem.GetGridPosition(
+                mouseWorld.GetRaycast(
+                    mouseWorld.MousePlaneLayerMask
+                ).Value.point
+            )
+        );
+    }
+}
